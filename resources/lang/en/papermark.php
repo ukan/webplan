494617ffabe77add08302a1d339aa53c2f6b9b7f
Ukan Saokani <khnz.97@gmail.com>
2017-04-14 23:11:12 +0700

    add language

---
+++ resources/lang/en/papermark.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    | AHLOO Language Lines
+    |
+    | @author Roni Yusuf <roni.y@smooets.com>
+    */
+
+   'delete_success' => 'Data deleted successfully!',
+   'delete_fail' => 'Failed to delete data!',
+
+   'save_success' => 'Data saved successfully!',
+   'save_fail' => 'Failed to save data!',
+
+];
